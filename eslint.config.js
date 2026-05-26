4848de625dab8ff0811d334aee01f01bd0c37393
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-05-26 16:49:09 -0700

    configure linting for frontend

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,36 @@
 import linted from "linted";
 
-export default linted();
+export default linted(
+  {
+    js: {
+      files: ["frontend/*.{js,mjs}"],
+    },
+    ts: {
+      files: [
+        "frontend/*.{ts,tsx}",
+        "frontend/src/**/*.{ts,tsx}",
+      ],
+    },
+    html: {
+      files: [
+        "frontend/*.{html}",
+        "frontend/src/**/*.{html}",
+      ],
+    },
+    css: {
+      files: [
+        "frontend/*.{css}",
+        "frontend/src/**/*.{css}",
+      ],
+    },
+    json: {
+      files: ["frontend/package.json"],
+    },
+    jsonc: {
+      files: [
+        "frontend/tsconfig.json",
+        "frontend/tsconfig.*.json",
+      ],
+    },
+  },
+);
