b75d0f727c4afb771fab31387cdad410d955cbb4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-05-27 18:42:53 -0700

    lint main

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,9 @@ import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
+
 if (!container) {
   throw new Error("Root element with id 'root' not found");
 }
 
-createRoot(container).render(
-  <App />
-);
+createRoot(container).render(<App />);
