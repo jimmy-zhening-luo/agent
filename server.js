ffda12542f42bfa47d6e993d205cd43330b875d0
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-05-27 18:57:35 -0700

    implement vite-express server with health check

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import ViteExpress from "vite-express";
+
+const app = express();
+
+app.get("/health", (_, res) => res.send("OK"));
+
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
