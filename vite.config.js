fa5ed2cd056618712d36f1de050cbe0ca3c716d6
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-05-27 20:14:54 -0700

    remove environment variable fallback for API target in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 
 const DEFAULT_API_TARGET = "https://agent-server.azurewebsites.net",
-apiTarget = process.env.VITE_API_URL ?? DEFAULT_API_TARGET;
+apiTarget =  DEFAULT_API_TARGET;
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
