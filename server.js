b4b2fc4c4cfb0604017100ef419c33ba4ab7acd4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-05-28 21:00:04 -0700

    Refactor static file middleware formatting for readability

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ import express from "express";
 import ViteExpress from "vite-express";
 
 const app = express();
+
 ViteExpress.config({ mode: "production" });
 
-app.use(express.static(
-  // directory named "dist" in the current directory
-  path.join(process.cwd(), "dist"),
-));
+app.use(
+  express.static(
+    path.join(
+      process.cwd(),
+      "dist",
+    ),
+  ),
+);
 
 app.get(
   "/health",
