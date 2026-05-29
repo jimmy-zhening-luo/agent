17883153ee3e1c47462b3ad1611d43723b5b09fd
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-05-28 17:02:48 -0700

    Add static file serving for production in server configuration

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+import path from "path";
 import express from "express";
 import ViteExpress from "vite-express";
 
 const app = express();
 ViteExpress.config({ mode: "production" });
+
+app.use(express.static(
+  // directory named "dist" in the current directory
+  path.join(process.cwd(), "dist"),
+));
 
 app.get(
   "/health",
