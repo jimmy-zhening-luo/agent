22a580dc1e9b53ed5514e4df57f9175cd2f7aed4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-05-28 17:15:46 -0700

    -unnecessary env directory configuration (already in root)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  envDir: path.resolve(__dirname),
   plugins: [react()],
 });
