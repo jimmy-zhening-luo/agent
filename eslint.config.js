ddd905e2835098c78d0adc520a1dc7be2dbfc695
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2026-06-04 01:35:24 -0700

    Simplify ESLint config by removing custom JS rules and ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,3 @@
 import linted from "linted";
 
-export default linted(
-  {
-    js: {
-      ignores: ["vite.config.js"],
-      rules: {
-        "no-undef": 0,
-      },
-    },
-  },
-);
+export default linted();
